3afbb769fec80707fc21c7fb4e28672cd111c7e1
node-gs <Gardesimpson@gmail.com>
2017-01-07 16:47:31 +1000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './app.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist/assets'),
+    publicPath: '/assets', // New
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader?importLoaders=1',
+          'postcss-loader',
+          'sass-loader',
+        ]
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './src'),  // New
+  },
+};
