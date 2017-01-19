baa7d839e976224b68ee5de9fee22df125ea446f
Gerard Simpson <Gerard.Simpson@dealersolutions.com.au>
2017-01-19 14:50:31 +1000

    haml set up

--- src/app.js
+++ src/app.js
@@ -1,9 +1,4 @@
-import moment from 'moment';
 
 import styles from './assets/style/css/style.css';
 import styles from './assets/style/sass/index.scss';
 
-
-var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
-console.log(rightNow);
-// "October 23rd 2016, 9:30:24 pm"
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.haml$/,
+        use: "haml-haml"
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
